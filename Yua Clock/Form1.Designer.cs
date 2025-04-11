299b3fee0642f7d84e3459b0792b2397a658efc0
calm evening <122962886+calmevening@users.noreply.github.com>
2025-04-12 04:11:53 +0800

    Add files via upload

---
+++ Yua Clock/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Yua_Clock
+{
+    partial class frm_yua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_yua));
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.tm_time = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_title.Font = new System.Drawing.Font("Lucida Handwriting", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_title.Location = new System.Drawing.Point(148, 9);
+            this.lbl_title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(285, 61);
+            this.lbl_title.TabIndex = 1;
+            this.lbl_title.Text = "Yua Clock";
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_time.Location = new System.Drawing.Point(61, 70);
+            this.lbl_time.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(79, 29);
+            this.lbl_time.TabIndex = 4;
+            this.lbl_time.Text = "label1";
+            // 
+            // tm_time
+            // 
+            this.tm_time.Enabled = true;
+            this.tm_time.Tick += new System.EventHandler(this.tm_time_Tick);
+            // 
+            // frm_yua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(597, 690);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbl_title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "frm_yua";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yua Clock";
+            this.Load += new System.EventHandler(this.frm_yua_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Timer tm_time;
+    }
+}
+
---
+++ Yua Clock/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Yua_Clock
+{
+    public partial class frm_yua : Form
+    {
+        public frm_yua()
+        {
+            InitializeComponent();
+        }
+
+        private void tm_time_Tick(object sender, EventArgs e)
+        {
+            lbl_time.Text = DateTime.Now.TimeOfDay + " " + DateTime.Now.ToLongDateString();
+            tm_time.Start();
+        }
+
+
+        private void frm_yua_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
